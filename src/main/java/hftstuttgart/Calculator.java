910f2d57f88d447a3582ac1237645a4b965965eb
Your Name <you@example.com>
2022-05-09 14:49:39 +0200

    add docker build

--- src/main/java/hftstuttgart/Calculator.java
+++ src/main/java/hftstuttgart/Calculator.java
@@ -11,9 +11,8 @@ public class Calculator {
     }    
 
     public int prod(int op1, int op2) {
-        int sum = 0;
-        sum *= op1;
-        sum *= op2;
-        return sum;
+        int op = 0;
+        op = op1 * op2;
+        return op;
     }    
 }
